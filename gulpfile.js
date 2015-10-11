c4396119aaffda6230ed679fb6368346dfc9be5d
Oskar Niburski <oskarniburski@gmail.com>
2015-10-11 16:34:26 -0400

    Sass not compiling with gulp task. Using less for now

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 gulp.task('js', function(){
     browserify('./public/js/src/app.jsx')
@@ -12,8 +13,15 @@ gulp.task('js', function(){
         .pipe(gulp.dest('public/js/build/'));
 });
 
+gulp.task('sass', function() {
+    gulp.src('public/stylesheets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/stylesheets/css'));
+})
+
 gulp.task('watch', function() {
     gulp.watch("public/js/src/**/*.jsx", ["js"])
+    gulp.watch("public/stylesheets/scss/*.scss", ["sass"])
 })
 
 gulp.task('default', ['js', 'watch']);
